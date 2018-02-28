98b643d8e513a3c515b8919bba54b0ab27dd1964
Chris J.M <chris@euchre.us>
2018-02-27 22:10:32 -0500

    Add command line options to rotate.py

--- rotate.py
+++ rotate.py
@@ -1,5 +1,5 @@
+#!/usr/bin/env python3
 # coding=UTF-8
-#!/usr/bin/python
 # -*- coding: UTF-8 -*-
 # vim: set fileencoding=UTF-8 :
 
@@ -10,19 +10,22 @@ from shutil import copyfile
 import datetime
 from dateutil.parser import *
 from dateutil.relativedelta import *
+import click
 
 
 # Creates a new nest list in the hist-list/<CITY> folder with a YYYY-MM-DD name
+
 
 outpath = "hist-list/"
 city = None
 date = None
 
+
 # Probably can share more code with sort.py methods, but they're slightly different
 def val_city(city=None):
-    while city is None or sort.validate_cityfile(city) is False:
-        city = sort.getcity()
-    return sort.choose_city(city), city[:-len(sort.ext)]
+    cpath = sort.choose_city(city+sort.ext) # path to the city's template file
+    city2 = cpath.split("/")[1][:-len(sort.ext)] # just the city name
+    return cpath, city2
 
 
 # checks that a directory exists and creates it if it does not
@@ -54,8 +57,7 @@ def str_int(str):
 # gets a date (also accepts relative dates like y-1, t+3, w+2)
 def getdate(date=None):
     today = datetime.datetime.today()
-    while date is None:
-        date = input("What is the date of the nest rotation (blank for today, " + str(today.date()) + ")? ")
+    while True:
         if date.strip() == "":
             return today.date()
         if date[0].lower() in "ymwt" and len(date) > 2 and date[1] in "+-" and str_int(date[1:]):
@@ -70,19 +72,22 @@ def getdate(date=None):
                 }.get(units, 0)
             return createdate.date()
         try:
-            date = parse(date)
+            return parse(date).date()
         except (ValueError, TypeError):
             print("Please enter a valid date.")
-            date = None
-    return date.date()
+            date = input("What is the date of the nest rotation (blank for today, " + str(today.date()) + ")? ")
 
+
+@click.command()
+@click.option('-d', '--date', default=str(datetime.datetime.today().date()), prompt="Date of nest shift", help="Date when the nest shift occured, can be absolute (YYYY-MM-DD) or relative (w+2)")
+@click.option('-c', '--city', '-f', prompt="City to rotate", help="The chosen city will be the one to have a fresh copy of its nest list created in its folder in hist-list/")
 
 # main method
-def main():
-    cfile, cname = val_city(city)
+def main(date, city):
+    cfile, cname = val_city(city.strip())
     odir = outpath + cname + "/"
     check_dir(odir)
-    d8 = str(getdate(date))
+    d8 = str(getdate(date.strip()))
     ofile = odir + d8 + sort.ext
     if os.path.exists(ofile):
         print("File " + ofile + " already exists.  Please delete it and run this program again if you want to reset it.")
@@ -92,4 +97,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(None, None)
